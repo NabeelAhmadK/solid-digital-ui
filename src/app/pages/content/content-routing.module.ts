500e14024f92650be8512202d6f7b0be1333db24
HAK <hoossainalik@gmail.com>
2021-01-26 04:16:10 +0500

    Updated Content Breadcrumb

--- src/app/pages/content/content-routing.module.ts
+++ src/app/pages/content/content-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
   {
     path: 'contents',
     component: ContentOverviewComponent,
-    data: { title: 'Contents', breadcrumb: 'Contents' },
+    data: { title: 'Content overview', breadcrumb: 'Content overview' },
     canActivate: [AuthGuard],
   },
   {
